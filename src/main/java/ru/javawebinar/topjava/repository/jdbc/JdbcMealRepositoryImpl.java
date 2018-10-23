93fdcd7e45e4731a14b40a77e9eb13315e540b6e
tral909 <indorm1992@mail.ru>
2018-10-24 02:05:40 +0300

    HW03 base 1,2 points, get and delete methods in 3 point impl

--- src/main/java/ru/javawebinar/topjava/repository/jdbc/JdbcMealRepositoryImpl.java
+++ src/main/java/ru/javawebinar/topjava/repository/jdbc/JdbcMealRepositoryImpl.java
@@ -1,5 +1,9 @@
 package ru.javawebinar.topjava.repository.jdbc;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.support.DataAccessUtils;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
 import ru.javawebinar.topjava.model.Meal;
 import ru.javawebinar.topjava.repository.MealRepository;
@@ -10,6 +14,18 @@ import java.util.List;
 @Repository
 public class JdbcMealRepositoryImpl implements MealRepository {
 
+    private static final RowMapper<Meal> ROW_MAPPER = (rs, rowNum) -> {
+        Meal meal = new Meal();
+        meal.setId(rs.getInt("id"));
+        meal.setDateTime(rs.getTimestamp("datetime").toLocalDateTime());
+        meal.setDescription(rs.getString("description"));
+        meal.setCalories(rs.getInt("calories"));
+        return meal;
+    };
+
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+
     @Override
     public Meal save(Meal meal, int userId) {
         return null;
@@ -17,12 +33,13 @@ public class JdbcMealRepositoryImpl implements MealRepository {
 
     @Override
     public boolean delete(int id, int userId) {
-        return false;
+        return jdbcTemplate.update("DELETE FROM meals WHERE id=? AND user_id=?", ROW_MAPPER, id, userId) != 0;
     }
 
     @Override
     public Meal get(int id, int userId) {
-        return null;
+        List<Meal> meals = jdbcTemplate.query("SELECT * FROM meals WHERE id=? AND user_id=?", ROW_MAPPER, id, userId);
+        return DataAccessUtils.singleResult(meals);
     }
 
     @Override
--- src/main/resources/db/populateDB.sql
+++ src/main/resources/db/populateDB.sql
@@ -1,3 +1,4 @@
+DELETE FROM meals;
 DELETE FROM user_roles;
 DELETE FROM users;
 ALTER SEQUENCE global_seq RESTART WITH 100000;
@@ -9,3 +10,8 @@ INSERT INTO users (name, email, password) VALUES
 INSERT INTO user_roles (role, user_id) VALUES
   ('ROLE_USER', 100000),
   ('ROLE_ADMIN', 100001);
+
+INSERT INTO meals (description, calories, user_id) VALUES
+  ('Завтрак', 600, 100000),
+  ('Обед', 1000, 100000),
+  ('Ужин', 400, 100000);
